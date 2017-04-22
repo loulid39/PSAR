6c3b2d7063839ac1c55920e42ca61128d67956da
loulid39 <loulid39@gmail.com>
2017-04-22 12:16:23 +0200

    avoir dernier niveau de cache automatique

--- src/libinject.c
+++ src/libinject.c
@@ -32,7 +32,9 @@ void initpapi()
 	events = malloc(sizeof(int)*NB_EVENTS);
 	values = malloc(sizeof(long long)*NB_EVENTS);
 	
-	events[0]=PAPI_L3_TCM;
+	if(PAPI_event_name_to_code("PAPI_L3_TCM",&events[0]) != PAPI_OK){
+		events[0]=PAPI_L2_TCM;
+	}
 	events[1]=PAPI_TOT_CYC;
 
 	
